5e4c3624f27d62905e97d2e2f76305acec7ca8a5
Kiiskinen Mauno TTK17SP <maunokiiskinen@kamk.fi>
2020-01-15 20:39:35 +0200

    Update gitignore and added basic player movement

---
+++ Hackaton/Assets/_Scripts/Player/PlayerInput.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+public class PlayerInput : MonoBehaviour
+{
+    public bool PressingDown;
+    public float Horizontal;
+    public float Vertical;
+
+
+    private void Update()
+    {
+        Horizontal = Input.GetAxis("Horizontal");
+        Vertical = Input.GetAxis("Vertical");
+        PressingDown = Input.GetMouseButton(0);
+    }
+
+    public Vector2 GetPlayerInput()
+    {
+        return new Vector2(Horizontal, Vertical).normalized;
+    }
+}
---
+++ Hackaton/Assets/_Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerInput), typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    #region Components
+
+    private PlayerInput input;
+    private Rigidbody2D rb;
+
+    #endregion
+
+    [Range(1.0f, 10.0f)]
+    [SerializeField] private float maxVelocity = 2.0f;
+    
+
+
+    private void Awake()
+    {
+        input = GetComponent<PlayerInput>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxVelocity);
+    }
+
+
+    // TODO:: Add horizontal movement
+    private void FixedUpdate()
+    {
+        if(input.PressingDown)
+        {
+            AddMovement(Vector2.up * -Physics2D.gravity * Time.deltaTime);
+        }
+    }
+
+    private void AddMovement(Vector2 force)
+    {
+        rb.velocity += force;
+    }
+
+}
